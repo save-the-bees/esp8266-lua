8ede06eb982454cb621d8dfe4b30ee6c2e95040f
Antonio P.P. Almeida <perusio@gmail.com>
2016-06-24 16:56:19 +0200

    * Code docs and secure port handling.

--- modules/coap_node.lua
+++ modules/coap_node.lua
@@ -41,6 +41,7 @@ local M  = {
   _DESCRIPTION = 'An implementation of a CoAP sensor node',
   _DEBUG = false, -- no debugging by default
   _PORT = 5683, -- CoAP default port
+  _SECURE_PORT = 5684, -- CoAP + DTLS (coaps) default port number
   _REQ_TYPE = coap.CON, -- by default use a confirmable request
   _METHOD = 'GET', -- default METHOD
   _SECURE = false -- default is normal CoAP (coap scheme in URLs)
@@ -72,31 +73,83 @@ local M  = {
 
 local mt = { __index = M }
 
-
 -- Allowed CoAP methods.
-local allowed_methods = '/GET/POST/PUT/DELETE/'
+local allowed_methods ={
+  client = '/GET/POST/PUT/DELETE/', -- for client
+  server = '/GET/POST/', -- for server
+}
 
+--- Checks the request type. Only CONfirmable or NON-confirmable
+--  requests are allowed.
+--
+-- @param number req_type
+--   The type of request: confirmable or non-confirmable.
+-- @return boolean
+--   true if request is CONfirmable or NON-confirmable.
+--   false otherwise.
 local function check_request_type(req_type)
   -- Currently only CONfirmable and NONconfirmable messages are
   -- supported.
   return coap.CON == req_type or coap.NON == req_type
 end
 
-local function check_method(method)
-  return find(allowed_methods, format('/%s/', upper(method))) ~= nil
+--- Checks the CoAP method for the client
+--
+-- @param string method
+--   CoAP method name.
+-- @param string allowed
+--   Allowed methods as a string.
+-- @return boolean
+--   true if method is allowed, false if not.
+local function check_method(method, allowed)
+  return find(allowed, format('/%s/', upper(method))) ~= nil
 end
 
+--- Builds a CoAP URL.
+--
+-- @param string address
+--   Can be an IPv4 address or a domain name.
+-- @param number port
+--   port number for the server to be queried.
+-- @param boolean is_secure
+--   true if scheme is coaps (CoAP + DTLS).
+-- @return string
+--   The URL.
 local function get_url(address, port, is_secure)
-  return format('%s://%s:%s', is_secure and 'coaps' or 'coap', address, port)
+  return format('%s://%s:%s',
+                is_secure and 'coaps' or 'coap',
+                address, port)
 end
 
-
+--- Checks if the address given is admissible. Either an
+--  IPv4 or a domain name.
+--
+-- @param string address
+--   IPv4 address or domain name.
+-- @return boolean
+--   true if address is valid. false if not.
 local function check_address(address)
   -- Is either an IPv4 IP address or a domain address.
   return find('%d+.%d+.%d+.%d+', address) ~= nil or find('[%a.-_]', address) ~= nil
 end
 
-
+--- Instantiates a CoAP client.
+--
+-- @param table self
+--   client object.
+-- @param string address
+--   IPv4 address or dimain name.
+-- @param number port.
+--   port number.
+-- @param string req_type
+--   CONfirmable or NON-confirmable requests.
+-- @param is_secure
+--   true if scheme is coaps, false if not.
+-- @param string payload
+--   The payload in serialized form. Makes
+--   sense only for POST and PUT.
+-- @return table
+--   The CoAP client object.
 function M.req(self, address, port, req_type, is_secure, payload)
 
   -- Set the client settings.
@@ -104,13 +157,14 @@ function M.req(self, address, port, req_type, is_secure, payload)
     -- Check the request type.
     req_t = req_type and check_request_type(req_type) or M._CONN,
     -- Check the port number.
-    p = port and type(port) == 'number' or M._PORT,
+    p = port and type(port) == 'number' or (is_secure and M._SECURE_PORT or M._PORT) ,
     -- Check the address.
     addr = address and check_address(address),
   }
-
   -- Instantiate the client.
   client.cc = coap.Client()
   return setmetatable(client, mt)
-
 end
+
+-- Return the module table.
+return M
